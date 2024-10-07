cda9e73266af80708fb6e947878c91478f219a03
samish24 <samishaddad@protonmail.com>
2024-10-07 04:15:30 +0200

    implementig and debugging orders & ordersRouter

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "./db/server.js";
 import categoryRouter from "./routes/categoryRouter.js";
+import ordersRouter from "./routes/ordersRouter.js";
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
 app.use("/category", categoryRouter);
+app.use("/order", ordersRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
